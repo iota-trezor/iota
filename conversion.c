1496a843e7abb45051e2b448e472b06959539a30
Bart Slinger <bartslinger@gmail.com>
2017-09-23 22:29:20 +0200

    trytes to trits conversion

--- conversion.c
+++ conversion.c
@@ -12,3 +12,40 @@ int trits_to_trytes(const trit_t trits_in[], tryte_t trytes_out[], uint32_t trit
     }
     return 0;
 }
+
+int trytes_to_trits(const tryte_t trytes_in[], trit_t trits_out[], uint32_t tryte_len)
+{
+    for (uint32_t i = 0; i < tryte_len; i++) {
+        int32_to_trits((int32_t)trytes_in[i], &trits_out[i*3], 3);
+    }
+    return 0;
+}
+
+int int32_to_trits(const int32_t value, trit_t trits_out[], uint8_t max_len)
+{
+    int32_t quotient = value / 3;
+    int8_t remainder = value % 3;
+    if (remainder == 2) {
+        quotient++;
+        remainder = -1;
+    }
+    if (remainder == -2) {
+        quotient--;
+        remainder = 1;
+    }
+    trits_out[0] = remainder;
+
+    if (quotient == 0) {
+        return 0;
+    }
+
+    int ret = 0;
+    if (max_len > 1) {
+        ret = int32_to_trits(quotient, &trits_out[1], max_len-1);
+    } else {
+        // Not sufficient trits available for representation
+        return -1;
+    }
+
+    return ret;
+}
--- conversion.h
+++ conversion.h
@@ -3,6 +3,9 @@
 
 #include "iota_types.h"
 
-int trits_to_trytes(const trit_t trits_in[], tryte_t trytes_out[], uint32_t trit_len);   
+int trits_to_trytes(const trit_t trits_in[], tryte_t trytes_out[], uint32_t trit_len);
+int trytes_to_trits(const tryte_t trytes_in[], trit_t trits_out[], uint32_t tryte_len);
+
+int int32_to_trits(const int32_t value, trit_t trits_out[], uint8_t max_len);
 
 #endif // CONVERSION_H
