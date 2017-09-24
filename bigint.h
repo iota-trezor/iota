e8dbfc62db7d645fd65e070ed53441d943273a17
Bart Slinger <bartslinger@gmail.com>
2017-09-24 13:22:41 +0200

    bigint add functions

---
+++ bigint.h
@@ -0,0 +1,8 @@
+#ifndef BIGINT_H
+#define BIGINT_H
+
+#include "iota_types.h"
+
+int bigint_add_int(int32_t bigint_in[], int32_t int_in, int32_t bigint_out[], uint8_t len);
+
+#endif // BIGINT_H
--- iota_types.h
+++ iota_types.h
@@ -3,8 +3,14 @@
 
 #include <string.h>
 #include <stdint.h>
+#include <stdbool.h>
 
 typedef int8_t trit_t;
 typedef int8_t tryte_t;
+
+struct int_bool_pair {
+	int32_t low;
+	bool hi;
+};
 
 #endif // IOTA_TYPES_H
