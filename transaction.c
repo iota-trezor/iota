9bf764461521cd0ca3c2a8a23f3ff4af8df817be
Bart Slinger <bartslinger@gmail.com>
2017-11-02 20:05:00 +0100

    refactor bundle hash calculation

--- transaction.c
+++ transaction.c
@@ -8,48 +8,47 @@ void get_bundle_essence_trits(const char address[], uint64_t value, const char t
 
 void get_bundle_essence_trits(const char address[], uint64_t value, const char tag[], uint64_t timestamp, uint32_t current_index, uint32_t last_index, trit_t bundle_essence_trits[])
 {
-    char bundle_essence[162];
-    memcpy(bundle_essence, address, 81);
+    // Set all to zero
+    memset(bundle_essence_trits, 0, 162*3);
 
+    // Address
     {
-        trit_t value_trits[81] = {0};
+        trit_t* address_trits = &bundle_essence_trits[0];
+        tryte_t address_trytes[81] = {0};
+        chars_to_trytes(address, address_trytes, 81);
+        trytes_to_trits(address_trytes, address_trits, 81);
+    }
+
+    // Value
+    {
+        trit_t* value_trits = &bundle_essence_trits[3*81];
         int32_to_trits(value, value_trits, 81);
-        tryte_t value_trytes[27] = {0};
-        trits_to_trytes(value_trits, value_trytes, 81);
-        trytes_to_chars(value_trytes, &bundle_essence[81], 27);
     }
 
-    memcpy(&bundle_essence[81+27], tag, 27);
-
+    // Tag
     {
-        trit_t timestamp_trits[27] = {0};
-        int32_to_trits(timestamp, timestamp_trits, 27);
-        tryte_t timestamp_trytes[9] = {0};
-        trits_to_trytes(timestamp_trits, timestamp_trytes, 27);
-        trytes_to_chars(timestamp_trytes, &bundle_essence[81+27+27], 9);
+        trit_t* tag_trits = &bundle_essence_trits[3*(81+27)];
+        tryte_t tag_trytes[27];
+        chars_to_trytes(tag, tag_trytes, 27);
+        trytes_to_trits(tag_trytes, tag_trits, 27);
     }
 
+    // Timestamp
     {
-        trit_t curr_idx_trits[27] = {0};
-        int32_to_trits(current_index, curr_idx_trits, 27);
-        tryte_t curr_idx_trytes[9] = {0};
-        trits_to_trytes(curr_idx_trits, curr_idx_trytes, 27);
-        trytes_to_chars(curr_idx_trytes, &bundle_essence[81+27+27+9], 9);
+        trit_t* timestamp_trits = &bundle_essence_trits[3*(81+27+27)];
+        int32_to_trits(timestamp, timestamp_trits, 3*9);
     }
 
+    // Current index
     {
-        trit_t last_idx_trits[27] = {0};
-        int32_to_trits(last_index, last_idx_trits, 27);
-        tryte_t last_idx_trytes[9] = {0};
-        trits_to_trytes(last_idx_trits, last_idx_trytes, 27);
-        trytes_to_chars(last_idx_trytes, &bundle_essence[81+27+27+9+9], 9);
+        trit_t* curr_idx_trits = &bundle_essence_trits[3*(81+27+27+9)];
+        int32_to_trits(current_index, curr_idx_trits, 3*9);
     }
 
+    // Last index
     {
-        // Convert to trits
-        tryte_t bundle_essence_trytes[162];
-        chars_to_trytes(bundle_essence, bundle_essence_trytes, 162);
-        trytes_to_trits(bundle_essence_trytes, bundle_essence_trits, 162);
+        trit_t* last_idx_trits = &bundle_essence_trits[3*(81+27+27+9+9)];
+        int32_to_trits(last_index, last_idx_trits, 3*9);
     }
 }
 
