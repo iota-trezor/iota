3ec7466211c2d06c43546bba3b92a813ecf84d2c
Bart Slinger <bartslinger@gmail.com>
2017-11-04 14:03:14 +0100

    include bugfix/work-around for normalized bundle-hash

--- transaction.c
+++ transaction.c
@@ -4,7 +4,26 @@
 #include <string.h>
 
 /* Private function(s) prototype(s) */
+void tag_increment(trit_t trits[]);
 void get_bundle_essence_trits(const char address[], uint64_t value, const char tag[], uint64_t timestamp, uint32_t current_index, uint32_t last_index, trit_t bundle_essence_trits[]);
+
+void tag_increment(trit_t trits[])
+{
+    // Add one
+    uint8_t offset = 0;
+    bool carry = true;
+    while(carry && offset < 81) {
+        trits[offset] = trits[offset] + 1;
+        if (trits[offset] > 1) {
+            trits[offset] = -1;
+        } else {
+            carry = false;
+        }
+        if (carry) {
+            offset++;
+        }
+    }
+}
 
 void get_bundle_essence_trits(const char address[], uint64_t value, const char tag[], uint64_t timestamp, uint32_t current_index, uint32_t last_index, trit_t bundle_essence_trits[])
 {
@@ -54,22 +73,53 @@ void get_bundle_essence_trits(const char address[], uint64_t value, const char t
 
 void calculate_standard_bundle_hash(const char from_addr[], const char to_addr[], const char remainder_addr[], uint64_t balance, uint64_t transaction_amount, const char tag[], uint64_t timestamp, tryte_t bundle_hash_out[])
 {
-    kerl_initialize();
+    bool valid_bundle = false;
+    char obsolete_tag_char[27];
+    memcpy(obsolete_tag_char, tag, 27);
 
-    // A standard bundle contains 4 transactions and is security level 2
-    trit_t bundle_essence_trits[162*3];
-    get_bundle_essence_trits(to_addr, transaction_amount, tag, timestamp, 0, 3, bundle_essence_trits);
-    kerl_absorb_trits(bundle_essence_trits, 486);
-    get_bundle_essence_trits(from_addr, -balance, tag, timestamp, 1, 3, bundle_essence_trits);
-    kerl_absorb_trits(bundle_essence_trits, 486);
-    get_bundle_essence_trits(from_addr, 0, tag, timestamp, 2, 3, bundle_essence_trits);
-    kerl_absorb_trits(bundle_essence_trits, 486);
-    get_bundle_essence_trits(remainder_addr, balance-transaction_amount, tag, timestamp, 3, 3, bundle_essence_trits);
-    kerl_absorb_trits(bundle_essence_trits, 486);
+    trit_t obsolete_tag_trits[81];
+    {
+        tryte_t obsolete_tag_trytes[27];
+        chars_to_trytes(obsolete_tag_char, obsolete_tag_trytes, 27);
+        trytes_to_trits(obsolete_tag_trytes, obsolete_tag_trits, 27);
+    }
 
-    trit_t trits_out[243];
-    kerl_squeeze_trits(trits_out, 243);
-    trits_to_trytes(trits_out, bundle_hash_out, 243);
+    while(!valid_bundle) {
+        kerl_initialize();
+
+        // A standard bundle contains 4 transactions and is security level 2
+        trit_t bundle_essence_trits[162*3];
+        get_bundle_essence_trits(to_addr, transaction_amount, obsolete_tag_char, timestamp, 0, 3, bundle_essence_trits);
+        kerl_absorb_trits(bundle_essence_trits, 486);
+        get_bundle_essence_trits(from_addr, -balance, tag, timestamp, 1, 3, bundle_essence_trits);
+        kerl_absorb_trits(bundle_essence_trits, 486);
+        get_bundle_essence_trits(from_addr, 0, tag, timestamp, 2, 3, bundle_essence_trits);
+        kerl_absorb_trits(bundle_essence_trits, 486);
+        get_bundle_essence_trits(remainder_addr, balance-transaction_amount, tag, timestamp, 3, 3, bundle_essence_trits);
+        kerl_absorb_trits(bundle_essence_trits, 486);
+
+        trit_t trits_out[243];
+        kerl_squeeze_trits(trits_out, 243);
+        trits_to_trytes(trits_out, bundle_hash_out, 243);
+
+        tryte_t normalized_hash[81];
+        normalize_hash(bundle_hash_out, normalized_hash);
+        bool has_m = false;
+        for (uint8_t i = 0; i < 81; i++) {
+            if (normalized_hash[i] == 13) {
+                has_m = true;
+            }
+        }
+
+        if (has_m) {
+            tag_increment(obsolete_tag_trits);
+            tryte_t obsolete_tag_trytes[27];
+            trits_to_trytes(obsolete_tag_trits, obsolete_tag_trytes, 81);
+            trytes_to_chars(obsolete_tag_trytes, obsolete_tag_char, 27);
+        } else {
+            valid_bundle = true;
+        }
+    }
 }
 
 void normalize_hash(const tryte_t hash_in[], tryte_t normalized_hash_out[])
